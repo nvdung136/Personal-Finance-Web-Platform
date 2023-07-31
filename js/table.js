4ccac143798fe23758b63c64a3b3dc0a79fc4969
nvdung136 <nvdung136@gmail.com>
2023-07-31 21:28:48 +0700

    Separate original files into Personal use and GitHub public repository

--- js/table.js
+++ js/table.js
@@ -1,5 +1,5 @@
 const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('../DB/PersonalFinance.db',sqlite.OPEN_READWRITE, (err) => {
+const db = new sqlite.Database('../DB/PersonalFinance_TestnGitHub.db',sqlite.OPEN_READWRITE, (err) => {
     if (err) return console.error(err);
 });
 
